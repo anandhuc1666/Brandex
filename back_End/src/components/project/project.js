1a000299d5694fe14cee23db795b2810d5648fc3
anandhuc1666 <anandhuc166@gmail.com>
2026-05-09 21:58:20 +0530

    updated the backend

--- back_End/src/components/project/project.js
+++ back_End/src/components/project/project.js
@@ -1,98 +1,133 @@
 import User from "../../models/SchemaUser.js ";
 import Project from "../../models/SchemaProject.js";
 
-
 // ...................................................New project ......................................................
 
 export const createProject = async (req, res) => {
-const { customer_name, user_name, customer_phone, product, customer_place, day, price, sticky } = req.body;
-const userId = req.user.id;
-try {
-    if(!userId){
-        return res.status(401).json({ message: "Unauthorized" });
-    } 
+  const {
+    customer_name,
+    user_name,
+    customer_phone,
+    product,
+    customer_place,
+    day,
+    price,
+    sticky,
+  } = req.body;
+  const userId = req.user.id;
+  try {
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
     const project = await Project.create({
-        userID: userId,
+      userID: userId,
+      customer_name,
+      user_name,
+      customer_phone,
+      product,
+      customer_place,
+      day,
+      price,
+      sticky,
+    });
+    res.status(201).json({ message: "Project created successfully", project });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating project", error: error.message });
+  }
+};
+
+// ....................................................Get all project ......................................................
+
+export const getAllProjects = async (req, res) => {
+  try {
+    const projects = await Project.find();
+    res
+      .status(200)
+      .json({ message: "Projects fetched successfully", projects });
+    console.log(projects);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching projects", error: error.message });
+  }
+};
+
+// ..................................................get by id project ......................................................
+
+export const getProjectById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const project = await Project.findById(id);
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+    res.status(200).json({ message: "Project fetched successfully", project });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching project", error: error.message });
+  }
+};
+
+// ....................................................update project ......................................................
+
+export const updateProject = async (req, res) => {
+  const { id } = req.params;
+  const {
+    customer_name,
+    user_name,
+    customer_phone,
+    product,
+    position,
+    customer_place,
+    day,
+    price,
+    sticky,
+  } = req.body;
+
+  try {
+    const project = await Project.findOneAndUpdate(
+      { _id: id },
+      {
         customer_name,
         user_name,
         customer_phone,
         product,
+        position,
         customer_place,
         day,
         price,
-        sticky
-    });
-    res.status(201).json({ message: "Project created successfully", project });
-} catch (error) {
-  res.status(500).json({ message: "Error creating project", error: error.message });  
-}
-}
-
-// ....................................................Get all project ......................................................
-
-
-export const getAllProjects = async (req, res) => {
-try {
-    const projects = await Project.find();
-    res.status(200).json({ message: "Projects fetched successfully", projects });
-    console.log(projects);
-} catch (error) {
-    res.status(500).json({ message: "Error fetching projects", error: error.message });
-}
-}
-
-// ..................................................get by id project ......................................................
-
-
-export const getProjectById = async (req, res) => {
-    const { id } = req.params;
-try {
-    const project = await Project.findById(id);
-    if (!project) {
-        return res.status(404).json({ message: "Project not found" });
-    }
-    res.status(200).json({ message: "Project fetched successfully", project });
-
-} catch (error) {
-    res.status(500).json({ message: "Error fetching project", error: error.message });
-}
-}
-
-// ....................................................update project ......................................................
-
-
-export const updateProject = async (req, res) => {
-const { id } = req.params;
-const { customer_name, user_name, customer_phone, product, position, customer_place, day, price, sticky } = req.body;
-
-try {
-    const project = await Project.findOneAndUpdate(
-        { _id: id },
-        { customer_name, user_name, customer_phone, product, position, customer_place, day, price, sticky },
-        { new: true }
+        sticky,
+      },
+      { new: true },
     );
 
-    if(!project){
-        return res.status(404).json({ message: "Project not found" });
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
     }
     res.status(200).json({ message: "Project updated successfully", project });
-} catch (error) {
-    res.status(500).json({ message: "Error updating project", error: error.message });
-}
-}
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error updating project", error: error.message });
+  }
+};
 
 // ....................................................delete project ......................................................
 
 export const deleteProject = async (req, res) => {
-const { id } = req.params;
-try {
+  const { id } = req.params;
+  try {
     const project = await Project.findByIdAndDelete(id);
     if (!project) {
-        return res.status(404).json({ message: "Project not found" });
+      return res.status(404).json({ message: "Project not found" });
     }
     res.status(200).json({ message: "Project deleted successfully", project });
-} catch (error) {
-    res.status(500).json({ message: "Error deleting project", error: error.message });
-}
-
-}
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error deleting project", error: error.message });
+  }
+};
