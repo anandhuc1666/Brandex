02bd20d9f67e96e5f39e72a1954843747e7b969d
anandhuc1666 <anandhuc166@gmail.com>
2026-06-26 16:57:57 +0530

    Fix transporter typo

--- back_End/src/components/Message/Msg.js
+++ back_End/src/components/Message/Msg.js
@@ -8,11 +8,13 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 transporter.verify((error, success) => {
